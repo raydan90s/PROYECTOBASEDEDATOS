61574c94de0c2617d14a4b301c5dbd824f8cf253
DhamarPatino <152041769+DhamarPatino@users.noreply.github.com>
2024-09-01 10:33:56 -0500

    cambios

---
+++ SQLDBControl/src/main/java/ec/espol/edu/sqldbcontrol/ReportesController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package ec.espol.edu.sqldbcontrol;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author DHAMAR
+ */
+public class ReportesController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
